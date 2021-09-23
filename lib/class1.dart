d5745a6695da2789c419a465c6b551f228a34d62
lifan <494360059@qq.com>
2021-09-23 14:55:22 +0800

    第一课

---
+++ lib/class1.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+
+void main() {
+  runApp(
+      const Center(
+        child: Text("hello word !", textDirection: TextDirection.ltr,),
+      )
+  );
+}
