509d6ab21d17e24155daf4a76f5c7f9882b0861f
lifan <494360059@qq.com>
2021-09-23 17:54:15 +0800

    ListView

--- lib/class1.dart
+++ lib/class1.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'model/post.dart';
 
 void main() => runApp(App());
 
@@ -7,22 +7,37 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("LiFan"),
-          elevation: 3,
-        ),
-        body: Home(),
-      ),
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
+      home: Home()
     );
   }
 
 }
 
+
 class Home extends StatelessWidget{
+
+  //这是一个方法 itemBuilder应该是一个高阶函数
+  Widget _listItemBuilder(BuildContext context, int index){
+    return Text(posts[index].title);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("LiFan"),
+        elevation: 3,
+      ),
+      body: ListView.builder(
+        itemCount: posts.length,
+        itemBuilder: _listItemBuilder,
+      )
+    );
+  }
+}
+
+class Hello extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return const Center(
