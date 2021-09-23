1c7164860cfab5f81c37f49e6ea597386aec0bd9
lifan <494360059@qq.com>
2021-09-23 15:33:34 +0800

    第二课

--- lib/class1.dart
+++ lib/class1.dart
@@ -1,11 +1,40 @@
 import 'package:flutter/material.dart';
 
 
+void main() => runApp(App());
 
-void main() {
-  runApp(
-      const Center(
-        child: Text("hello word !", textDirection: TextDirection.ltr,),
-      )
-  );
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("LiFan"),
+          elevation: 3,
+        ),
+        body: Home(),
+      ),
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+    );
+  }
+
+}
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        "hello word",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+            fontSize: 20,
+            color: Colors.blueAccent
+        ),
+      ),
+    );
+  }
+  
 }
